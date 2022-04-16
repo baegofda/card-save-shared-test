f2d70cc3e5670f70179c830e6b8d4ad3667c19bb
baegofda <molly.daleo3o@gmail.com>
2022-04-16 16:54:55 +0900

    feat: 이미지저장, 카카오공유하기 테스트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import html2canvas from "html2canvas";
+import { useEffect } from "react";
+const { Kakao } = window;
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    initKakao();
+  }, []);
+
+  const initKakao = () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init("e41efab98dfe045c9d7ce45f8b320444");
+      }
+    }
+  };
+
+  function sendLink() {
+    Kakao.Link.createDefaultButton({
+      container: ".card-shard" /*버튼 아이디*/,
+      objectType:
+        "feed" /*공유 타입. 다른 방식도 있는데 공식 홈페이지에서 확인이 가능하다.*/,
+      content: {
+        title: "테스트!!",
+        description: "하하하",
+        imageUrl: "함께 첨부될 이미지의 url",
+        link: {
+          webUrl: "https://baegofda.github.io/card-save-shard-test/",
+        },
+      },
+    });
+  }
+
+  const onSave = (uri, filename) => {
+    console.log("onSaveAs");
+    var link = document.createElement("a");
+    document.body.appendChild(link);
+    link.href = uri;
+    link.download = filename;
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const onCapture = () => {
+    console.log("onCapture");
+    html2canvas(document.querySelector(".grade-card")).then((canvas) => {
+      onSave(canvas.toDataURL("image/png"), "grade-card.jpg");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="card-save" type="button" onClick={onCapture}>
+        이미지 저장하기
+      </button>
+      <button className="card-shard" type="button" onClick={sendLink}>
+        카톡 공유
+      </button>
+      <button className="card-save" type="button">
+        페이스북 공유
+      </button>
+      <div className="grade-card">이미지 저장을 해봅시다</div>
     </div>
   );
-}
+};
 
 export default App;
